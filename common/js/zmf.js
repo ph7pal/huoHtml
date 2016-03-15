49827d4a3ed968472f2f84d8dc65c5f29d4a9518
ph7pal <ph7pal@qq.com>
2016-03-15 18:03:22 +0800

    backup

--- common/js/zmf.js
+++ common/js/zmf.js
@@ -1,12 +1,12 @@
 function bindLink() {
     $('.ui-grid-full li,.ui-list li,.ui-row li,.ui-avatar,.ui-col,.ui-slider-content li,.ui-btn-wrap').unbind('click').click(function () {
         if ($(this).attr('data-href')) {
-            if($(this).attr('data-href')=='#'){
+            if ($(this).attr('data-href') == '#') {
                 alert('请在data-href中指定链接');
                 return false;
-            }else{
+            } else {
                 location.href = $(this).attr('data-href');
-            }            
+            }
         }
     });
 }
@@ -35,40 +35,61 @@ $('.ui-searchbar-cancel').tap(function () {
     $('.ui-searchbar-wrap').removeClass('focus');
 });
 /*导航条上菜单按钮*/
-$('.fa-list').unbind('tap').tap(function(){
-    var _dom=$('#header-scroller');
-    var _dom2=$('#body-cover');
-    if(_dom.css('display')=='none'){
-        _dom.show().animate({top:0},800); 
-        if(!_dom2.length){
+$('.fa-list').unbind('tap').tap(function () {
+    var _dom = $('#header-scroller');
+    var _dom2 = $('#body-cover');
+    if (_dom.css('display') == 'none') {
+        _dom.show().animate({top: 0}, 800);
+        if (!_dom2.length) {
             $('body').append('<div class="body-cover" id="body-cover"></div>');
         }
-    }else{
-        _dom.hide().animate({top:'-2000'},300); 
+    } else {
+        _dom.hide().animate({top: '-2000'}, 300);
         _dom2.remove();
-    }      
+    }
 })
 /*购房直通车的条件筛选*/
 $(".condition-item").unbind('tap').tap(function () {
     var _dom = $(this);
     var _type = _dom.attr('data-type');
     var _value = _dom.attr('data-value');
+    var _multi = _dom.attr('data-multi');
     if (!_type) {
         return false;
     }
-    _dom.parent('.ui-btn-wrap').children('.condition-item').each(function () {
-        $(this).removeClass('active');
-    });
-    _dom.addClass('active');
-    $('#condition-' + _type).val(_value);
+    if (_type == 'area') {
+        if (_dom.hasClass('active')) {
+            _dom.removeClass('active');
+            $('#condition-area').remove();
+        } else {
+            if (_multi != 1) {
+                _dom.parent('.ui-btn-wrap').children('.condition-item').each(function () {
+                    $(this).removeClass('active');
+                });
+            }
+            _dom.addClass('active');
+            if ($('#condition-area').length) {
+                $('#condition-area').val(_value);
+            } else {
+                var _html = '<input type="hidden" id="condition-area" name="area" value="' + _value + '"/>';
+                $('#selected-condtions').append(_html);
+            }
+        }
+    } else {
+        if (_dom.hasClass('active')) {
+            _dom.removeClass('active');
+            $('#condition-' + _type + '-' + _value).remove();
+        } else {
+            _dom.addClass('active');
+            var _html = '<input type="hidden" id="condition-' + _type + '-' + _value + '" name="' + _type + '[]" value="' + _value + '"/>';
+            $('#selected-condtions').append(_html);
+        }
+    }
 });
 $('.condition-reset').unbind('tap').tap(function () {
     $(".condition-item").each(function () {
         $(this).removeClass('active');
     });
-    $('#condition-area').val('');
-    $('#condition-type').val('');
-    $('#condition-apartment').val('');
-    $('#condition-tag').val('');
+    $('#selected-condtions').html('');
 })
 bindLink();
